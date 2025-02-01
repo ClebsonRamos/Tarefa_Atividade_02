4b2144471e9be0969acb19220d18475ffdb79a73
Clebson Ramos <clebsonramoss@gmail.com>
2025-02-01 18:25:18 -0300

    Versão 03 do código. O código foi otimizado para usar apenas 1 função de callback e alguns erros de lógica foram sanados.

--- Tarefa_Atividade_02.c
+++ Tarefa_Atividade_02.c
@@ -10,12 +10,13 @@
 
 //-----PROTÓTIPOS DAS FUNÇÕES-----
 void ativacao_dos_leds(void);
-int64_t funcao_de_callback_01(alarm_id_t id, void *user_data);
-int64_t funcao_de_callback_02(alarm_id_t id, void *user_data);
-int64_t funcao_de_callback_03(alarm_id_t id, void *user_data);
+int64_t funcao_de_callback(alarm_id_t id, void *user_data);
 void inicializacao_dos_pinos(void);
 
 //-----VARIÁVEIS GLOBAIS-----
+// Variável para registrar qual LED está sendo manipulado.
+// 1 - LED azul; 2 - LED verde; 3 - LED vermelho.
+static volatile uint registro_leds = 0;
 static volatile bool botao_pressionado = false; // Variável para registro do pressionamento do botão.
 static volatile bool alarme_ativado = false; // Variável para registro da ativação dos alarmes.
 
@@ -35,14 +36,15 @@ int main(void){
                 ativacao_dos_leds();
                 if(!alarme_ativado){
                     // Ativação dos alarmes temporizados.
-                    add_alarm_in_ms(3000, funcao_de_callback_01, NULL, false);
-                    add_alarm_in_ms(6000, funcao_de_callback_02, NULL, false);
-                    add_alarm_in_ms(9000, funcao_de_callback_03, NULL, false);
+                    add_alarm_in_ms(3000, funcao_de_callback, NULL, false);
+                    add_alarm_in_ms(6000, funcao_de_callback, NULL, false);
+                    add_alarm_in_ms(9000, funcao_de_callback, NULL, false);
                     // Registro da ativação dos alarmes.
                     alarme_ativado = !alarme_ativado;
                 }
             }
         }
+        sleep_ms(1); // Atraso de 1 milissegundo para reduzir o uso do microcontrolador.
     }
 
     return 0;
@@ -50,25 +52,32 @@ int main(void){
 
 //-----FUNÇÕES COMPLEMENTARES-----
 void ativacao_dos_leds(void){
+    // Ativa todos os 3 LEDs.
     gpio_put(PINO_LED_AZUL, true);
     gpio_put(PINO_LED_VERDE, true);
     gpio_put(PINO_LED_VERMELHO, true);
 }
 
-int64_t funcao_de_callback_01(alarm_id_t id, void *user_data){
-    gpio_put(PINO_LED_AZUL, false);
-    return 0;
-}
+int64_t funcao_de_callback(alarm_id_t id, void *user_data){
+    switch(registro_leds){
+        case 0: // Apaga o LED azul.
+            gpio_put(PINO_LED_AZUL, false);
+            break;
+        case 1: // Apaga o LED verde.
+            gpio_put(PINO_LED_VERDE, false);
+            break;
+        case 2: // Apaga o LED vermelho e alterna os valores das variáveis registradoras.
+            gpio_put(PINO_LED_VERMELHO, false);
+            botao_pressionado = !botao_pressionado;
+            alarme_ativado = !alarme_ativado;
+            break;
+    }
+    
+    // Incremento na variável de registro do LED da vez.
+    registro_leds++;
+    if(registro_leds == 3) // Caso o valor seja igual a 3, o registro é reiniciado.
+        registro_leds = 0;
 
-int64_t funcao_de_callback_02(alarm_id_t id, void *user_data){
-    gpio_put(PINO_LED_VERDE, false);
-    return 0;
-}
-
-int64_t funcao_de_callback_03(alarm_id_t id, void *user_data){
-    gpio_put(PINO_LED_VERMELHO, false);
-    botao_pressionado = !botao_pressionado;
-    alarme_ativado = !alarme_ativado;
     return 0;
 }
 
