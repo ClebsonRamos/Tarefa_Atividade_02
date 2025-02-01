e7c21aeda2eb869d5fbbe31af9a003187d34eae2
Clebson Ramos <clebsonramoss@gmail.com>
2025-02-01 18:11:40 -0300

    Versão 02 do código. Ajuste da lógica e criação de 3 funções de callback.

--- Tarefa_Atividade_02.c
+++ Tarefa_Atividade_02.c
@@ -10,26 +10,37 @@
 
 //-----PROTÓTIPOS DAS FUNÇÕES-----
 void ativacao_dos_leds(void);
-int64_t funcao_de_callback(alarm_id_t id, void *user_data);
+int64_t funcao_de_callback_01(alarm_id_t id, void *user_data);
+int64_t funcao_de_callback_02(alarm_id_t id, void *user_data);
+int64_t funcao_de_callback_03(alarm_id_t id, void *user_data);
 void inicializacao_dos_pinos(void);
 
 //-----VARIÁVEIS GLOBAIS-----
-absolute_time_t temporizador;
-static volatile uint led_aceso = 0;
-static volatile bool botao_pressionado = false;
+static volatile bool botao_pressionado = false; // Variável para registro do pressionamento do botão.
+static volatile bool alarme_ativado = false; // Variável para registro da ativação dos alarmes.
 
 //-----FUNÇÃO PRINCIPAL-----
 int main(void){
-    inicializacao_dos_pinos();
+    inicializacao_dos_pinos(); // Inicialização dos pinos dos LEDs e do botão utilizados.
 
     while(true){
+        // Desvio condicional para testar se o botão foi pressionado.
+        // A variável botao_pressionado registra se o botão foi efetivamente pressionado, após o tratamento do debounce.
         if(gpio_get(PINO_BOTAO_A) == 0 && !botao_pressionado){
-            sleep_ms(50);
-            if(gpio_get(PINO_BOTAO_A) == 0){
+            sleep_ms(50); // Atraso de 50 milissegundos para o debounce.
+            if(gpio_get(PINO_BOTAO_A) == 0){ // Testa novamente de o botão foi pressionado.
+                // Registra o pressionamento do botão, para evitar pressionamentos consecutivos durante a execução da função de callback.
+                botao_pressionado = !botao_pressionado;
+                // Ativa todos os LEDs.
                 ativacao_dos_leds();
-                add_alarm_in_ms(3000, funcao_de_callback, NULL, false);
-                add_alarm_in_ms(6000, funcao_de_callback, NULL, false);
-                add_alarm_in_ms(9000, funcao_de_callback, NULL, false);
+                if(!alarme_ativado){
+                    // Ativação dos alarmes temporizados.
+                    add_alarm_in_ms(3000, funcao_de_callback_01, NULL, false);
+                    add_alarm_in_ms(6000, funcao_de_callback_02, NULL, false);
+                    add_alarm_in_ms(9000, funcao_de_callback_03, NULL, false);
+                    // Registro da ativação dos alarmes.
+                    alarme_ativado = !alarme_ativado;
+                }
             }
         }
     }
@@ -44,22 +55,20 @@ void ativacao_dos_leds(void){
     gpio_put(PINO_LED_VERMELHO, true);
 }
 
-int64_t funcao_de_callback(alarm_id_t id, void *user_data){
-    switch(led_aceso){
-        case 0:
-            gpio_put(PINO_LED_AZUL, false);
-            break;
-        case 1:
-            gpio_put(PINO_LED_VERDE, false);
-            break;
-        case 2:
-            gpio_put(PINO_LED_VERMELHO, false);
-            botao_pressionado = !botao_pressionado;
-            break;
-    }
+int64_t funcao_de_callback_01(alarm_id_t id, void *user_data){
+    gpio_put(PINO_LED_AZUL, false);
+    return 0;
+}
 
-    led_aceso--;
+int64_t funcao_de_callback_02(alarm_id_t id, void *user_data){
+    gpio_put(PINO_LED_VERDE, false);
+    return 0;
+}
 
+int64_t funcao_de_callback_03(alarm_id_t id, void *user_data){
+    gpio_put(PINO_LED_VERMELHO, false);
+    botao_pressionado = !botao_pressionado;
+    alarme_ativado = !alarme_ativado;
     return 0;
 }
 
